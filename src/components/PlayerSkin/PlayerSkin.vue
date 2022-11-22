e8d6690c2de909d0c857506b3786dc13321c7a95
Viktor Persson <viktor.persson@arcsin.se>
2022-11-22 18:31:06 +0100

    Allow drawing on player skin canvas.

--- src/components/PlayerSkin/PlayerSkin.vue
+++ src/components/PlayerSkin/PlayerSkin.vue
@@ -35,7 +35,7 @@ const handleCustomSkinChange = (event: Event) => {
 const setPlayerTextureByFile = (file: File) => {
   const reader = new FileReader();
 
-  reader.addEventListener("load", async function () {
+  reader.addEventListener("load", async () => {
     if (!viewer.model || 0 === viewer.model.materials.length) {
       return;
     }
@@ -50,16 +50,88 @@ const setPlayerTextureByFile = (file: File) => {
       texture
     );
     store.skinTextureURI = reader.result as string;
+    renderPlayerSkinToCanvas();
   });
 
   reader.readAsDataURL(file);
 };
 
 let viewer: ModelViewerElement;
+let playerCanvas: HTMLCanvasElement;
+let ctx: CanvasRenderingContext2D | null;
+let playerCanvasBoundingBox: DOMRect;
+let playerImage: HTMLImageElement;
+
+const renderPlayerSkinToCanvas = () => {
+  const canvasDrawTimeout = 25;
+  window.setTimeout(() => {
+    ctx?.drawImage(
+      playerImage,
+      0,
+      0,
+      playerImage.width,
+      playerImage.height,
+      0,
+      0,
+      playerCanvas.width,
+      playerCanvas.height
+    );
+  }, canvasDrawTimeout);
+};
 
 onMounted(() => {
-  viewer = document.getElementById("player") as ModelViewerElement;
+  viewer = document.getElementById("PlayerViewer") as ModelViewerElement;
+  playerImage = document.getElementById("PlayerImage") as HTMLImageElement;
+  playerCanvas = document.getElementById("PlayerCanvas") as HTMLCanvasElement;
+  ctx = playerCanvas.getContext("2d");
+  playerCanvasBoundingBox = playerCanvas.getBoundingClientRect();
+  renderPlayerSkinToCanvas();
 });
+
+// new position from mouse event
+let canvasPos = { x: 0, y: 0 };
+
+const updateCanvasPosition = (event: MouseEvent) => {
+  canvasPos.x = event.clientX - playerCanvasBoundingBox.x;
+  canvasPos.y = event.clientY - playerCanvasBoundingBox.y;
+};
+
+const drawLineOnCanvas = (event: MouseEvent) => {
+  const MOUSE_PRIMARY_BUTTON = 1;
+
+  if (event.buttons !== MOUSE_PRIMARY_BUTTON) {
+    return;
+  }
+
+  let lastCanvasPos = Object.assign({}, { ...canvasPos });
+  updateCanvasPosition(event);
+  drawOnCanvas(lastCanvasPos.x, lastCanvasPos.y, canvasPos.x, canvasPos.y);
+};
+
+const drawDotOnCanvas = (event: MouseEvent) => {
+  updateCanvasPosition(event);
+  drawOnCanvas(canvasPos.x, canvasPos.y, canvasPos.x, canvasPos.y);
+};
+
+const drawOnCanvas = (x0: number, y0: number, x1: number, y1: number) => {
+  if (!ctx) {
+    return;
+  }
+
+  ctx.lineWidth = 5;
+  ctx.lineCap = "round";
+  ctx.strokeStyle = "#ff0000";
+
+  ctx.beginPath();
+  ctx.moveTo(x0, y0);
+  ctx.lineTo(x1, y1);
+  ctx.stroke();
+};
+
+const clearCanvasDrawing = () => {
+  ctx?.clearRect(0, 0, playerCanvas.width, playerCanvas.height);
+  renderPlayerSkinToCanvas();
+};
 </script>
 
 <template>
@@ -77,7 +149,7 @@ onMounted(() => {
             class="bg-gradient-to-b from-transparent via-white border-2 col-span-3"
           >
             <model-viewer
-              id="player"
+              id="PlayerViewer"
               :src="`${baseUrl}/assets/models/playerout.gltf`"
               camera-controls
               disable-zoom
@@ -90,11 +162,34 @@ onMounted(() => {
             </model-viewer>
           </div>
           <div
-            class="col-span-4 debug self-center"
+            class="col-span-4 self-center"
+            style="width: 512px"
             @drop="handleDrop"
             @dragover.prevent
           >
-            <img :src="store.skinTextureURI" alt="Player Skin" class="w-full" />
+            <canvas
+              id="PlayerCanvas"
+              class="border bg-white cursor-crosshair"
+              height="386"
+              width="512"
+              @mousedown="drawDotOnCanvas"
+              @mouseenter="updateCanvasPosition"
+              @mousemove="drawLineOnCanvas"
+            ></canvas>
+            <img
+              id="PlayerImage"
+              :src="store.skinTextureURI"
+              alt="Player Skin"
+            />
+
+            <div class="p-2 bg-gray-300">
+              <button
+                @click="clearCanvasDrawing"
+                class="border border-gray-400 hover:bg-red-100 rounded p-2 bg-gray-100 shadow"
+              >
+                Clear drawing
+              </button>
+            </div>
           </div>
 
           <div class="col-span-3 ml-8">
