0bbb801187774b39ce9291215e67268b9b5cc828
Viktor Persson <viktor.persson@arcsin.se>
2022-11-23 16:21:45 +0100

    Indicate currently selected color in color palette.

--- src/components/PlayerSkin/ColorPalette.vue
+++ src/components/PlayerSkin/ColorPalette.vue
@@ -1,4 +1,6 @@
 <script lang="ts" setup>
+import { reactive } from "vue";
+
 interface PaletteProps {
   onColorChange: (color: string) => void;
 }
@@ -16,14 +18,26 @@ const colors = [
   "#ff00ff",
   "#00ffff",
 ];
+
+const store = reactive({
+  currentColor: "#ff0000",
+});
+
+const onChange = (color: string) => {
+  store.currentColor = color;
+  props.onColorChange(color);
+};
 </script>
 <template>
-  <div class="flex gap-1">
+  <div class="flex gap-1 items-center">
     <div
       v-for="color in colors"
-      class="w-8 h-8 border border-black hover:border-4 cursor-pointer transition shadow"
+      class="w-8 h-8 border border-black hover:border-4 cursor-pointer transition shadow flex items-center justify-center"
       :style="{ backgroundColor: color }"
-      @click="() => props.onColorChange(color)"
+      @click="() => onChange(color)"
+      :class="
+        color === store.currentColor ? 'w-10 h-10 border-black border-4' : ''
+      "
     ></div>
   </div>
 </template>
