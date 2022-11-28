1f60d00b99a700ecaccee3dd212a413d7d10661f
Viktor Persson <viktor.persson@arcsin.se>
2022-11-29 00:52:52 +0100

    Lint util.

--- src/components/util.ts
+++ src/components/util.ts
@@ -15,7 +15,7 @@ export function slugify(text: string): string {
     .toLowerCase() // Convert the string to lowercase letters
     .trim() // Remove whitespace from both sides of a string (optional)
     .replace(/\s+/g, "-") // Replace spaces with -
-    .replace(/[^\w\-]+/g, "") // Remove all non-word chars
+    .replace(/[^\w-]+/g, "") // Remove all non-word chars
     .replace(/_/g, "-") // Replace _ with -
     .replace(/--+/g, "-") // Replace multiple - with single -
     .replace(/-$/g, ""); // Remove trailing -
