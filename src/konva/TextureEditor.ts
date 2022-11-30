2e5c39d75296c2cfd1c87b487af341ca58d06f69
Viktor Persson <viktor.persson@arcsin.se>
2022-11-30 22:35:01 +0100

    Remove obsolete comment [skip ci].

--- src/konva/TextureEditor.ts
+++ src/konva/TextureEditor.ts
@@ -213,10 +213,10 @@ export class TextureEditor {
 }
 
 function ColorizeFilter(imageData: ImageData) {
-  // make all pixels opaque 100%
   const data = imageData.data;
   const pixelCount = data.length;
   let brightness;
+
   for (let i = 0; i < pixelCount; i += 4) {
     brightness = 0.34 * data[i] + 0.5 * data[i + 1] + 0.16 * data[i + 2];
     data[i] += brightness; // red
