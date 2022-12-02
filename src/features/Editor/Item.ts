5f3f0b7cfd7f43b13100713cd741e0b9673db463
Viktor Persson <viktor.persson@arcsin.se>
2022-12-02 14:22:24 +0100

    Refactor item

--- src/features/Editor/Item.ts
+++ src/features/Editor/Item.ts
@@ -7,7 +7,6 @@ import { Model } from "../../pkg/quake/models";
 
 // types
 export interface Item {
-  id: string;
   category: string;
   model: Model;
   viewerOrientation: number;
@@ -36,7 +35,7 @@ export function itemToEditorSettings(
 
 export function itemToViewerSettings(item: Item): ModelViewerSettings {
   return {
-    containerID: slugify(`viewer ${item.id}`),
+    containerID: slugify(`viewer ${item.model.filename}`),
     modelPath: modelFilenamePath(item.model.filename),
     texturePath: modelFilenamePath(item.model.textures[0].filename),
   };
@@ -45,19 +44,16 @@ export function itemToViewerSettings(item: Item): ModelViewerSettings {
 // items
 const GreenArmor: Item = {
   category: "Armors",
-  id: slugify(quakeModels.GreenArmor.name),
   model: quakeModels.GreenArmor,
   viewerOrientation: 45,
 };
 const YellowArmor = {
   category: "Armors",
-  id: slugify("Yellow Armor"),
   model: quakeModels.YellowArmor,
   viewerOrientation: 45,
 };
 const RedArmor = {
   category: "Armors",
-  id: slugify(quakeModels.RedArmor.name),
   model: quakeModels.RedArmor,
   viewerOrientation: 45,
 };
@@ -66,7 +62,6 @@ export const armors: Item[] = [GreenArmor, YellowArmor, RedArmor];
 // Units
 export const player: Item = {
   category: "Units",
-  id: slugify(quakeModels.Player.name),
   model: quakeModels.Player,
   viewerOrientation: 45,
 };
@@ -76,42 +71,36 @@ const weaponOrientation = 45;
 
 const SuperShotgun: Item = {
   category: "Weapons",
-  id: slugify(quakeModels.SuperShotgun.name),
   model: quakeModels.SuperShotgun,
   viewerOrientation: weaponOrientation,
 };
 
 const NailGun: Item = {
   category: "Weapons",
-  id: slugify(quakeModels.Nailgun.name),
   model: quakeModels.Nailgun,
   viewerOrientation: weaponOrientation,
 };
 
 const SuperNailGun: Item = {
   category: "Weapons",
-  id: slugify(quakeModels.SuperNailgun.name),
   model: quakeModels.SuperNailgun,
   viewerOrientation: weaponOrientation,
 };
 
 const GrenadeLauncher: Item = {
   category: "Weapons",
-  id: slugify(quakeModels.GrenadeLauncher.name),
   model: quakeModels.GrenadeLauncher,
   viewerOrientation: weaponOrientation,
 };
 
 const RocketLauncher: Item = {
   category: "Weapons",
-  id: slugify(quakeModels.RocketLauncher.name),
   model: quakeModels.RocketLauncher,
   viewerOrientation: weaponOrientation,
 };
 
 const LightningGun: Item = {
   category: "Weapons",
-  id: slugify(quakeModels.LightningGun.name),
   model: quakeModels.LightningGun,
   viewerOrientation: weaponOrientation,
 };
@@ -127,14 +116,12 @@ export const weapons: Item[] = [
 
 const Grenade: Item = {
   category: "Projectiles",
-  id: slugify(quakeModels.Grenade.name),
   model: quakeModels.Grenade,
   viewerOrientation: 90,
 };
 
 const Missile: Item = {
   category: "Projectiles",
-  id: slugify(quakeModels.Missile.name),
   model: quakeModels.Missile,
   viewerOrientation: 90,
 };
@@ -142,19 +129,16 @@ export const projectiles: Item[] = [Grenade, Missile];
 
 const Quad: Item = {
   category: "Powerups",
-  id: slugify(quakeModels.Quad.name),
   model: quakeModels.Quad,
   viewerOrientation: 30,
 };
 const Pent: Item = {
   category: "Powerups",
-  id: slugify(quakeModels.Pent.name),
   model: quakeModels.Pent,
   viewerOrientation: 30,
 };
 const Ring: Item = {
   category: "Powerups",
-  id: slugify(quakeModels.Ring.name),
   model: quakeModels.Ring,
   viewerOrientation: 30,
 };
@@ -165,7 +149,6 @@ export const powerups: Item[] = [Quad, Pent, Ring];
 
 export const backpack: Item = {
   category: "Misc",
-  id: slugify(quakeModels.Backpack.name),
   model: quakeModels.Backpack,
   viewerOrientation: 30,
 };
@@ -174,21 +157,18 @@ export const backpack: Item = {
 
 const MegaHealth = {
   category: "Health packs",
-  id: slugify(quakeModels.MegaHealth.name),
   model: quakeModels.MegaHealth,
   viewerOrientation: 45,
 };
 
 const LargeHealth = {
   category: "Health packs",
-  id: slugify(quakeModels.HealthLarge.name),
   model: quakeModels.HealthLarge,
   viewerOrientation: 45,
 };
 
 const SmallHealth = {
   category: "Health packs",
-  id: slugify(quakeModels.HealthSmall.name),
   model: quakeModels.HealthSmall,
   viewerOrientation: 45,
 };
@@ -199,56 +179,48 @@ export const healthPacks: Item[] = [SmallHealth, LargeHealth, MegaHealth];
 
 const SmallShells = {
   category: "Ammo",
-  id: slugify(quakeModels.ShellsSmall.name),
   model: quakeModels.ShellsSmall,
   viewerOrientation: 45,
 };
 
 const LargeShells = {
   category: "Ammo",
-  id: slugify(quakeModels.ShellsLarge.name),
   model: quakeModels.ShellsLarge,
   viewerOrientation: 45,
 };
 
 const SmallNails = {
   category: "Ammo",
-  id: slugify(quakeModels.NailsSmall.name),
   model: quakeModels.NailsSmall,
   viewerOrientation: 45,
 };
 
 const LargeNails = {
   category: "Ammo",
-  id: slugify(quakeModels.NailsLarge.name),
   model: quakeModels.NailsLarge,
   viewerOrientation: 45,
 };
 
 const SmallRockets = {
   category: "Ammo",
-  id: slugify(quakeModels.RocketsSmall.name),
   model: quakeModels.RocketsSmall,
   viewerOrientation: 45,
 };
 
 const LargeRockets = {
   category: "Ammo",
-  id: slugify(quakeModels.RocketsLarge.name),
   model: quakeModels.RocketsLarge,
   viewerOrientation: 45,
 };
 
 const SmallCells = {
   category: "Ammo",
-  id: slugify(quakeModels.CellsSmall.name),
   model: quakeModels.CellsSmall,
   viewerOrientation: 45,
 };
 
 const LargeCells = {
   category: "Ammo",
-  id: slugify(quakeModels.CellsLarge.name),
   model: quakeModels.CellsLarge,
   viewerOrientation: 45,
 };
