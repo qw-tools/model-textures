e586214d1668e7ae6d990ce1b9325f92e02120a1
Viktor Persson <viktor.persson@arcsin.se>
2022-12-02 16:43:02 +0100

    Move colirize filter to separate file.

--- src/pkg/konva/TextureEditor.ts
+++ src/pkg/konva/TextureEditor.ts
@@ -10,6 +10,7 @@ import { Brush, getDefaultBrush } from "./Brush";
 import { nullOperation } from "../stringUtil";
 import { saveAs } from "file-saver";
 import { CssFilterSettings } from "../CssFilter";
+import { ColorizeFilter } from "./filters";
 
 export interface TextureEditorSettings {
   containerID: string;
@@ -206,18 +207,5 @@ export class TextureEditor {
     this.modelTextureOutline.isVisible()
       ? this.modelTextureOutline.hide()
       : this.modelTextureOutline.show();
-  }
-}
-
-function ColorizeFilter(imageData: ImageData) {
-  const data = imageData.data;
-  const pixelCount = data.length;
-  let brightness;
-
-  for (let i = 0; i < pixelCount; i += 4) {
-    brightness = 0.34 * data[i] + 0.5 * data[i + 1] + 0.16 * data[i + 2];
-    data[i] += brightness; // red
-    data[i + 1] += 0; // green
-    data[i + 2] += 0; // blue
   }
 }
