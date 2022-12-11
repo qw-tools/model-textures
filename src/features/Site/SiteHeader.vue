c31ccd84ebf796fe6777965f50015f1d0f92baee
Viktor Persson <viktor.persson@arcsin.se>
2022-12-11 18:20:48 +0100

    Update site header style.

--- src/features/Site/SiteHeader.vue
+++ src/features/Site/SiteHeader.vue
@@ -12,14 +12,14 @@ const pages = [
 <template>
   <div class="bg-gray-800">
     <div class="container h-12 items-center text-white text-sm">
-      <div class="flex items-center h-12 space-x-2">
-        <div class="font-bold">QuakeWorld Graphics</div>
+      <div class="flex items-center h-12">
+        <div class="font-bold mr-6">QuakeWorld Graphics</div>
 
         <a
           v-for="page in pages"
           :key="page.url"
           :href="`./${page.url}.html`"
-          class="px-4 opacity-80 h-full items-center flex hover:text-white hover:opacity-100 transition-colors"
+          class="px-2 opacity-80 h-full items-center flex hover:text-white hover:opacity-100 transition-colors"
           :class="
             page.url === props.currentPage
               ? 'font-bold text-green-300 opacity-100'
