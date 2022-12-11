cf6bad72dbcdd8ca2c6c743eeb917182b21abf6c
Viktor Persson <viktor.persson@arcsin.se>
2022-12-11 23:06:25 +0100

    Refactor editor character.

--- src/features/CharsetEditor/pixi/EditorCharacter.ts
+++ src/features/CharsetEditor/pixi/EditorCharacter.ts
@@ -5,19 +5,14 @@ import { OutlineFilter } from "pixi-filters";
 export class EditorCharacter extends PIXI.Text {
   char: Character;
   isSelected: boolean = false;
-  onSelect: (char: EditorCharacter) => void;
-  onDeselect: (char: EditorCharacter) => void;
+  onSelect: (char: EditorCharacter) => void = () => {};
+  onDeselect: (char: EditorCharacter) => void = () => {};
 
   constructor(char: Character) {
     super(char.value);
     this.char = char;
 
     this.interactive = true;
-    this.onSelect = (char: EditorCharacter) => {
-    };
-    this.onDeselect = (char: EditorCharacter) => {
-    };
-
     this.on("click", this.toggleSelect);
     this.on("mouseenter", this.select);
     this.on("mouseout", this.deselect);
