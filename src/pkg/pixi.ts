f6d70e4fa136f1d864f2517b8ddf51792aaf7edd
Viktor Persson <viktor.persson@arcsin.se>
2023-01-02 22:55:03 +0100

    Add method to determine if a canvas has transparent pixels.

--- src/pkg/pixi.ts
+++ src/pkg/pixi.ts
@@ -30,3 +30,23 @@ export async function createOutline(
 
   return PIXI.Sprite.from(renderTexture);
 }
+
+export function hasTransparency(canvas: HTMLCanvasElement): boolean {
+  const ctx = canvas.getContext("2d");
+
+  if (!ctx) {
+    console.log("no context");
+    return false;
+  }
+
+  const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+  const data = imageData.data;
+
+  for (let i = 3; i < data.length; i += 4) {
+    if (data[i] === 0) {
+      return true;
+    }
+  }
+
+  return false;
+}
