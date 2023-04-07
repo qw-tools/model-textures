97320bf0784707b65515a3089815e1cf6ef5c2df
Viktor Persson <viktor.persson@arcsin.se>
2023-04-07 17:20:01 +0200

    Refactor cursor creation.

--- src/features/ModelTextureEditor/pixi/Cursor.ts
+++ src/features/ModelTextureEditor/pixi/Cursor.ts
@@ -1,23 +1,21 @@
-import * as PIXI from "pixi.js";
+import { Color, Graphics, IRenderer } from "pixi.js";
 import { Brush } from "./brush";
 
-export class Cursor extends PIXI.Graphics {
-  toCss(renderer: PIXI.Renderer, brush: Brush): string {
-    console.log("toCss", brush);
-    this.clear();
+export function createCssCursor(renderer: IRenderer, brush: Brush): string {
+  const graphics = new Graphics();
+  const sideLength = brush.size / 2;
 
-    const sideLength = brush.size / 2;
+  graphics.lineStyle(2, new Color(brush.color));
 
-    this.lineStyle(2, new PIXI.Color(brush.color));
-
-    if (brush.shape === "circle") {
-      this.drawCircle(0, 0, sideLength);
-    } else if (brush.shape === "square") {
-      this.drawRect(0, 0, brush.size, brush.size);
-    }
-
-    const cursorCanvas = renderer.extract.canvas(this);
-
-    return `url(${cursorCanvas.toDataURL?.()}) ${sideLength} ${sideLength}, auto`;
+  if (brush.shape === "circle") {
+    graphics.drawCircle(0, 0, sideLength);
+  } else if (brush.shape === "square") {
+    graphics.drawRect(0, 0, brush.size, brush.size);
   }
+
+  const cursorCanvas = renderer.extract.canvas(graphics);
+
+  const css = `url(${cursorCanvas.toDataURL?.()}) ${sideLength} ${sideLength}, auto`;
+  console.log("css", css);
+  return css;
 }
